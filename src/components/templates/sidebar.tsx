32902b60a897085feaed9a9985f789ac5ee1ed9e
kionys <kiwon7271@naver.com>
2024-10-21 03:07:34 +0900

    add: 사이드바 컴포넌트 추가

--- src/components/templates/sidebar.tsx
+++ src/components/templates/sidebar.tsx
@@ -1,37 +1,183 @@
 "use client";
 
+import { createClient } from "@supabase/supabase-js";
+import axios from "axios";
 import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
+import { ChangeEvent, useRef } from "react";
+import Avatar from "../elements/avatar";
 
 const Sidebar = () => {
-  const { data: session, status } = useSession();
+  const { data: session, status, update } = useSession();
+
+  const imgRef = useRef<HTMLInputElement | null>(null);
+
+  const onChangeImage = async (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const imageData = e.currentTarget.files;
+
+    if (imageData && imageData[0]) {
+      if (imageData[0].size > 1.5 * 1024 * 1024) {
+        window.alert("썸네일이미지의 크기는 1.5MB를 초과할 수 없습니다.");
+      } else if (
+        imageData[0].type === "image/jpg" ||
+        imageData[0].type === "image/jpeg" ||
+        imageData[0].type === "image/png"
+      ) {
+        // 업로드 함수 호출
+        await uploadFile(imageData[0]);
+      } else {
+        window.alert("형식이 잘못되었습니다");
+      }
+      return;
+    }
+  };
+
+  // 회원 썸네일 변경
+  // const uploadFile = async (file: File) => {
+  //   if (!file || !session || status !== "authenticated") return;
+
+  //   // 파일 이름에서 확장자를 추출
+  //   const fileExtension = file.name.split(".").pop(); // 파일 확장자 추출
+
+  //   // 유저 ID와 타임값, 랜덤 문자열로 경로 생성
+  //   const timestamp = Date.now(); // 현재 시간
+  //   const randomSuffix = Math.random().toString(36).substring(2, 8); // 랜덤 문자열 생성
+  //   const newFilePath = `${session?.user.id}/${timestamp}_${randomSuffix}.${fileExtension}`; // 경로: userId/타임값_랜덤.확장자
+
+  //   // 기존 이미지 파일 제거 로직 추가
+  //   const existingImagePath = session?.user?.image?.split("/").pop(); // 기존 이미지 이름 추출
+  //   const existingImageFolder = session?.user?.id; // 기존 이미지 폴더 경로
+
+  //   // Supabase 클라이언트 생성
+  //   const supabaseClient = createClient(
+  //     process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  //     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+  //   );
+
+  //   if (existingImagePath) {
+  //     // 기존 이미지 파일 삭제
+  //     try {
+  //       await supabaseClient.storage
+  //         .from(process.env.NEXT_PUBLIC_STORAGE_BUCKET as string)
+  //         .remove([`${existingImageFolder}/${existingImagePath}`]); // 기존 이미지 파일 삭제
+  //     } catch {}
+  //   }
+
+  //   // 새로운 이미지 파일 업로드
+  //   supabaseClient.storage.from(process.env.NEXT_PUBLIC_STORAGE_BUCKET as string).upload(newFilePath, file);
+
+  //   // 이미지 URL 생성
+  //   const requestPath = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${process.env.NEXT_PUBLIC_STORAGE_BUCKET}/${newFilePath}`;
+  //   try {
+  //     const data = { id: session?.user.id, image: requestPath };
+  //     await axios
+  //       .put(`/api/profile`, data)
+  //       .then(res => update({ image: res.data.image }))
+  //       .catch(e => console.log(e));
+  //   } catch {}
+  // };
+  // 회원 썸네일 변경
+  const uploadFile = async (file: File) => {
+    // 파일이 없거나 세션이 유효하지 않으면 종료
+    if (!file) return;
+    if (!session) {
+      window.alert("로그인이 필요합니다.");
+      return;
+    }
+    if (status !== "authenticated") return;
+
+    // 파일 크기 체크
+    if (file.size > 1.5 * 1024 * 1024) {
+      window.alert("파일 크기는 1.5MB를 초과할 수 없습니다.");
+      return;
+    }
+
+    // 파일 형식 체크
+    if (!["image/jpeg", "image/png", "image/jpg"].includes(file.type)) {
+      window.alert("지원하지 않는 파일 형식입니다.");
+      return;
+    }
+
+    // 파일 이름에서 확장자를 추출
+    const fileExtension = file.name.split(".").pop(); // 파일 확장자 추출
+
+    // 유저 ID와 타임값, 랜덤 문자열로 경로 생성
+    const timestamp = Date.now(); // 현재 시간
+    const randomSuffix = Math.random().toString(36).substring(2, 8); // 랜덤 문자열 생성
+    const newFilePath = `${session?.user.id}/${timestamp}_${randomSuffix}.${fileExtension}`; // 경로: userId/타임값_랜덤.확장자
+
+    // 기존 이미지 파일 제거 로직 추가
+    const existingImagePath = session?.user?.image?.split("/").pop(); // 기존 이미지 이름 추출
+    const existingImageFolder = session?.user?.id; // 기존 이미지 폴더 경로
+
+    // Supabase 클라이언트 생성
+    const supabaseClient = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+    );
+
+    // 기존 이미지 파일 삭제
+    if (existingImagePath) {
+      const { error: deleteError } = await supabaseClient.storage
+        .from(process.env.NEXT_PUBLIC_STORAGE_BUCKET as string)
+        .remove([`${existingImageFolder}/${existingImagePath}`]);
+
+      if (deleteError) {
+        window.alert("기존 이미지 삭제에 실패했습니다.");
+        return;
+      }
+    }
+
+    // 새로운 이미지 파일 업로드
+    const { error } = await supabaseClient.storage
+      .from(process.env.NEXT_PUBLIC_STORAGE_BUCKET as string)
+      .upload(newFilePath, file);
+
+    if (error) {
+      window.alert("파일 업로드에 실패했습니다.");
+      return;
+    }
+
+    // 이미지 URL 생성
+    const requestPath = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${process.env.NEXT_PUBLIC_STORAGE_BUCKET}/${newFilePath}`;
+    const data = { id: session?.user.id, image: requestPath };
+
+    try {
+      const res = await axios.put(`/api/profile`, data);
+      update({ image: res.data.image });
+    } catch (error) {
+      window.alert("프로필 업데이트에 실패했습니다.");
+    }
+  };
 
   return (
     <div
-      className={`hidden sm:block w-[300px] shrink-0 bg-white h-[100vh] border-r-[1px] overflow-hidden transition-transform duration-200`}
+      className={`hidden sm:block w-[300px] shrink-0 bg-white h-[100vh] overflow-hidden transition-transform duration-200`}
     >
-      <div className="w-full h-10 flex font-semibold justify-center text-xl mt-8 border-b-[1px]">Gather Pets</div>
-      <div className="w-full flex flex-col items-center overflow-scroll h-[95vh] gap-5">
-        <div className="relative w-fit h-fit rounded-full overflow-hidden mt-8">
-          <Image
-            className="object-cover"
-            src={session?.user?.image ? session.user.image : "/images/avatar.jpeg"}
-            alt="썸네일 이미지"
-            width={80}
-            height={80}
-            priority // 로딩 우선순위 설정
-          />
+      <div className="w-full flex flex-col items-center overflow-scroll h-[100vh] gap-3">
+        <div
+          className="relative w-20 h-20 rounded-full overflow-hidden mt-8 cursor-pointer group"
+          onClick={() => imgRef.current && imgRef.current.click()}
+        >
+          <Avatar src={session?.user?.image} />
+          <input type="file" ref={imgRef} onChange={onChangeImage} accept="image/jpg, image/jpeg, image/png" hidden />
+          <div className="absolute inset-0 flex items-center justify-center text-white font-semibold text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20">
+            편집
+          </div>
         </div>
+
         <div className="flex flex-col gap-1 items-center">
           <div className="text-sm font-normal">{session?.user?.name}</div>
           <div className="text-md text-gray-500 font-light">{session?.user?.email}</div>
         </div>
-        <button
-          className="border px-3 py-2 border-blue-500 rounded-lg bg-blue-500 text-white"
-          onClick={() => signOut()}
-        >
-          로그아웃
-        </button>
+        <div className="flex">
+          <button
+            className="border px-3 py-2 border-blue-500 rounded-lg bg-blue-500 text-white"
+            onClick={() => signOut()}
+          >
+            로그아웃
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/templates/sidenav.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FiBox } from "react-icons/fi";
+import Avatar from "../elements/avatar";
+
+const Sidenav = () => {
+  const [open, setOpen] = useState<boolean>(true);
+  const { data: session } = useSession();
+
+  return (
+    <div
+      className={`hidden sm:flex flex-col items-center bg-gray-200 h-screen p-5 pt-8 rounded-r-2xl ${
+        open ? "w-72 flex-shrink-0" : "w-10"
+      } duration-300 sticky top-0 left-0`}
+    >
+      <div
+        className="w-10 h-14 flex items-center justify-center rounded-xl absolute -right-6 top-9 bg-gray-200 cursor-pointer"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? <FaArrowLeft className="h-4 text-gray-500" /> : <FaArrowRight className="h-4 text-gray-500" />}
+      </div>
+
+      {open && (
+        <>
+          {/* 프로필 */}
+          <div className="mt-16">
+            <Avatar src={session?.user?.image} className="w-28 h-28" />
+          </div>
+
+          {/* 내 정보 */}
+          <div className="mt-5 flex flex-col w-full items-center justify-center">
+            <div className="font-semibold text-xl">
+              {session?.user.name}({session?.user.id})
+            </div>
+            <div className="text-gray-400">{session?.user.email}</div>
+          </div>
+
+          {/* 게시글, 팔로워, 팔로잉 */}
+          <div className="mt-10 flex items-center justify-center text-sm">
+            <div className="flex flex-col gap-1 items-center justify-center cursor-pointer hover:opacity-60">
+              <p className="font-medium text-lg text-gray-800">126</p>
+              <p className="text-gray-400 text-sm">게시글</p>
+            </div>
+            <Divider />
+            <div className="flex flex-col gap-1 items-center justify-center cursor-pointer hover:opacity-60">
+              <p className="font-medium text-lg text-gray-800">1.8k</p>
+              <p className="text-gray-400 text-sm">팔로워</p>
+            </div>
+            <Divider />
+            <div className="flex flex-col gap-1 items-center justify-center cursor-pointer hover:opacity-60">
+              <p className="font-medium text-lg text-gray-800">558</p>
+              <p className="text-gray-400 text-sm">팔로잉</p>
+            </div>
+          </div>
+
+          {/* 메뉴 */}
+          <div className="mt-10 flex flex-col gap-3 w-full">
+            <Link href="/" className="px-4 py-3 hover:bg-gray-300 rounded-md w-full text-md flex items-center gap-3">
+              <FiBox />
+              <p>홈</p>
+            </Link>
+            <Link href="/" className="px-4 py-3 hover:bg-gray-300 rounded-md w-full text-md flex items-center gap-3">
+              <FiBox />
+              <p>피드</p>
+            </Link>
+            <Link href="/" className="px-4 py-3 hover:bg-gray-300 rounded-md w-full text-md flex items-center gap-3">
+              <FiBox />
+              <p>알림</p>
+            </Link>
+            <Link
+              href="/users/mypage"
+              className="px-4 py-3 hover:bg-gray-300 rounded-md w-full text-md flex items-center gap-3"
+            >
+              <FiBox />
+              <p>마이페이지</p>
+            </Link>
+            <DividerX />
+            <Link href="/" className="px-4 py-3 hover:bg-gray-300 rounded-md w-full text-md flex items-center gap-3">
+              <FiBox />
+              <p>설정</p>
+            </Link>
+            <button
+              className="px-4 py-3 hover:bg-gray-300 rounded-md w-full text-md flex items-center gap-3"
+              onClick={() => signOut()}
+            >
+              <FiBox />
+              <p>로그아웃</p>
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+export default Sidenav;
+
+const Divider = () => {
+  return (
+    <div
+      className="w-[54px] h-0 border-t-[0.5px] border-transparent border-gray-500"
+      style={{ transform: "rotate(-90deg)", gap: "0px" }}
+    ></div>
+  );
+};
+const DividerX = () => {
+  return (
+    <div
+      className="w-[full] h-0 border-t-[0.5px] border-transparent border-gray-500"
+      style={{ transform: "rotate(0deg)", gap: "0px" }}
+    ></div>
+  );
+};
