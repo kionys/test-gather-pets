fe02b7b7f262d24dd609215652a538dce69c27f4
kionys <kiwon7271@naver.com>
2024-10-21 03:04:24 +0900

    update: query client provider 적용

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
-
+import { useState } from "react";
 interface IPropsNextProvider {
   children: React.ReactNode;
 }
 
 export const NextProvider = ({ children }: IPropsNextProvider) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
 };
 
 export const NextLayout = ({ children }: IPropsNextProvider) => {
